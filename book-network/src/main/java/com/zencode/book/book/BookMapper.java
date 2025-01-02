62be484aa8aad7d5264ab6a5210824bbd51d6976
Muhammad Zein Fikry Lazuardi <zenfikry@gmail.com>
2025-01-02 14:46:43 +0700

    adding read file utils

--- book-network/src/main/java/com/zencode/book/book/BookMapper.java
+++ book-network/src/main/java/com/zencode/book/book/BookMapper.java
@@ -1,5 +1,6 @@
 package com.zencode.book.book;
 
+import com.zencode.book.file.FileUtils;
 import com.zencode.book.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class BookMapper {
                 .archived(book.isArchived())
                 .shareable(book.isShareable())
                 .owner(book.getOwner().fullname())
-//                .cover()
+                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
                 .build();
     }
 
